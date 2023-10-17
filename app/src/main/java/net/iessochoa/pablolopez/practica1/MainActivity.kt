2e67fb4dd7bf62df381ab7252e12938cdf9a0ea3
moevius237 <116665285+moevius237@users.noreply.github.com>
2023-10-17 02:46:37 +0200

    Quinto Commit,Guardar

--- app/src/main/java/net/iessochoa/pablolopez/practica1/MainActivity.kt
+++ app/src/main/java/net/iessochoa/pablolopez/practica1/MainActivity.kt
@@ -2,10 +2,18 @@ package net.iessochoa.pablolopez.practica1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etNombre:EditText
+    private lateinit var btAceptar:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //buscamos los views
+        etNombre=findViewById(R.id.etNombre)
+        btAceptar=findViewById(R.id.btAceptar)
     }
 }
---
+++ app/src/main/java/net/iessochoa/pablolopez/practica1/SaludoActivity.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.pablolopez.practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaludoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+    }
+}
